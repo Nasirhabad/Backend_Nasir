052c46f81e56ca4913a6e9f9ea1f2c0176aeda43
Nasir <newhope790@gmail.com>
2024-07-31 01:37:34 +0700

    submit task of Time complexity

---
+++ 04_Task_Time_complexity_and_space/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// time complexity
+	// 1. O(n)
+	// 2. O(1)
+
+	// O(1)
+	length := 4
+	width := 9
+	area := length * width // 1 basic operation
+
+	fmt.Println(area)
+}
