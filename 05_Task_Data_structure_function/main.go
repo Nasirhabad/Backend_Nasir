d680513aeed8c7355dd8d04fdd8f634bcddf8ae3
Nasir <newhope790@gmail.com>
2024-08-02 03:27:39 +0700

    Example from array_data structure

---
+++ 05_Task_Data_structure_function/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// array-> store many item in espacific size (fixed size)
+	//approach one
+	var numbers [5]int
+
+	//inset items
+	numbers[0] = 12
+	numbers[1] = 14
+	numbers[2] = 46
+	numbers[3] = 50
+
+	// approach 1 for accessing array -> regular for loop
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+
+	}
+	// 2nd approad for accessing  array -> for range
+	for idx, val := range numbers {
+		fmt.Println("the index: ", idx, "the value: ", val)
+	}
+
+}
