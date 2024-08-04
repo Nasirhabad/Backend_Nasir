aaa6f6a0e4e102e1b2d47d22b05fc5b0ad72543a
Nasir <newhope790@gmail.com>
2024-08-05 02:14:35 +0700

    new

--- 05_Task_Data_structure_function/main.go
+++ 05_Task_Data_structure_function/main.go
@@ -5,23 +5,26 @@ import "fmt"
 func main() {
 	// array-> store many item in espacific size (fixed size)
 	//approach one
-	var numbers [5]int
+	// var numbers [5]int
 
 	//inset items
-	numbers[0] = 12
-	numbers[1] = 14
-	numbers[2] = 46
-	numbers[3] = 50
+	// numbers[0] = 12
+	// numbers[1] = 14
+	// numbers[2] = 46
+	// numbers[3] = 50
 
-	// approach 1 for accessing array -> regular for loop
-	// more costomizable (epecify which index)
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
+	// // approach 1 for accessing array -> regular for loop
+	// // more costomizable (epecify which index)
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
 
-	}
-	// 2nd approad for accessing  array -> for range
-	for idx, val := range numbers {
-		fmt.Println("the index: ", idx, "the value: ", val)
-	}
+	// }
+	// // 2nd approad for accessing  array -> for range
+	// for idx, val := range numbers {
+	// 	fmt.Println("the index: ", idx, "the value: ", val)
+	// }
+
+	// slice
+
 
 }
